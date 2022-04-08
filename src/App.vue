38176cc614a814ca26a5481459f0aee521f288e9
lrnbr21 <laraschertel@gmail.com>
2022-04-08 14:54:45 +0200

    apis implemented

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Header/>
+    <router-view></router-view>
+    <Footer/>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Footer
+  },
+
+  data() {
+      return {
+      recipeList: [],
+      showAddRecipe: false
+       }
+   },
+
+
 }
 </script>
 
 <style>
+
+
+body {
+  background: url('./images/backgroundMain.jpg') no-repeat center center fixed;
+  background-size: cover;
+  overflow-y: scroll;
+   
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
